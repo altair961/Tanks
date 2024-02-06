b1dbc77d0d2bd5d34aefbcbbfc92f9652d186c78
altair961 <altair89@list.ru>
2024-02-06 17:45:07 +0300

    A window was added.

---
+++ TanksOnCSharp/MyGame.cs
@@ -0,0 +1,37 @@
+﻿using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+
+namespace TanksOnCSharp
+{
+    public class MyGame
+    {
+        IWindow window;
+        GL? Gl;
+        public MyGame()
+        {
+            window = Window.Create(WindowOptions.Default with
+            {
+                Title = "Tanks"
+            });
+
+            window.Render += Window_Render;
+            window.Load += Window_Load;
+        }
+
+        private void Window_Load()
+        {
+            Gl = GL.GetApi(window);
+        }
+
+        private void Window_Render(double obj)
+        {
+            Gl?.ClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+            Gl?.Clear(ClearBufferMask.ColorBufferBit);
+        }
+
+        public void Start()
+        {
+            window.Run();
+        }
+    }
+}
