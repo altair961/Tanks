61bf8df16e0d56559ed53737d5b28803cdd972d6
altair961 <altair89@list.ru>
2024-02-09 17:17:59 +0300

    Silk.Net app was added.

---
+++ Tanks/Tanks/MyGame.cs
@@ -0,0 +1,34 @@
+﻿using Silk.NET.Core.Contexts;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+
+namespace Tanks
+{
+    public class MyGame
+    {
+        IWindow window;
+        GL Gl;
+        public MyGame()
+        {
+            window = Window.Create(WindowOptions.Default);
+            window.Render += Window_Render;
+            window.Load += Window_Load;
+        }
+
+        private void Window_Load()
+        {
+            Gl = GL.GetApi(window);
+        }
+
+        private void Window_Render(double obj)
+        {
+            Gl.ClearColor(0.5f, 0.5f, 0.2f, 0.1f);
+            Gl.Clear(ClearBufferMask.ColorBufferBit);
+        }
+
+        public void Start() 
+        {
+            window.Run();
+        }
+    }
+}
---
+++ Tanks/Tanks/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Tanks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var game = new MyGame();
+            game.Start();
+        }
+    }
+}
