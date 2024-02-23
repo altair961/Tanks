1300deadfb41b7b15fa561ef0615988d5d6413d7
altair961 <altair89@list.ru>
2024-02-23 16:29:29 +0300

    IdempotentInitialize call was added to Application's constructor.

--- Tanks/Tanks/Application.cs
+++ Tanks/Tanks/Application.cs
@@ -8,6 +8,8 @@ namespace Tanks
         {
             if (game == null)
                 throw new ArgumentNullException("game", "Please provide game object when making instance of Application class.");
+
+            game.IdempotentInitialize();
         }
     }
 }
