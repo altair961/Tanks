c5053ce66a2a90dcf0dee92420766763956ea491
altair961 <altair89@list.ru>
2024-02-29 07:34:24 +0300

    Window was registered as IWindow for unit-tests.

--- Tanks/Tanks.UnitTests/IoCContainer.cs
+++ Tanks/Tanks.UnitTests/IoCContainer.cs
@@ -12,6 +12,7 @@ namespace Tanks.UnitTests
                 builder.RegisterType<Application>();
                 builder.RegisterType<Game>().As<IGame>();
                 builder.RegisterType<Renderer>().As<IRender>();
+                builder.RegisterType<Window>().As<IWindow>();
                 return builder.Build();
             }
         }
