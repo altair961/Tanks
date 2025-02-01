eac15bdfe985e746e02d392eca0f0ef327a3ec57
altair961 <altair89@list.ru>
2025-02-01 14:41:03 +0300

    Define basic window

--- SilkNetOpenGL/SilkNetTriangle/Program.cs
+++ SilkNetOpenGL/SilkNetTriangle/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace SilkNetTriangle
+﻿using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+namespace SilkNetTriangle
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static IWindow window;
+
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var options = WindowOptions.Default;
+            options.Size = new Vector2D<int>(800, 600);
+            options.Title = "LearnOpenGL with Silk.NET";
+
+            window = Window.Create(options);
+            window.Run();
+            window.Dispose();
         }
     }
 }
