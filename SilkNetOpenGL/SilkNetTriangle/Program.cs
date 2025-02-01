c3a0dc757329e158f4aac1d0f19ceec23ed041b1
altair961 <altair89@list.ru>
2025-02-01 14:27:46 +0300

    Create SilkNetOpenGL empty solution

---
+++ SilkNetOpenGL/SilkNetTriangle/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SilkNetTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
