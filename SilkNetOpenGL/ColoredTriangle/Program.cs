b08108de9312c3246d2ec620c9d6d0f1bf5f33f6
altair961 <altair89@list.ru>
2025-02-08 15:01:28 +0300

    Draw Black triangle

--- SilkNetOpenGL/ColoredTriangle/Program.cs
+++ SilkNetOpenGL/ColoredTriangle/Program.cs
@@ -1,9 +1,27 @@
-﻿using Silk.NET.Maths;
+﻿using System.Drawing;
+using Silk.NET.Maths;
 using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
 
-namespace DrawPoint
+namespace ColoredTriangle
 {
+    public enum VertexAttribute
+    {
+        Position,
+        Color
+    }
+    public class VertexPositionColor
+    {
+        public VertexPositionColor(float x, float y, float z, Color color)
+        {
+            Position = [x, y, z];
+            Color = color;
+        }
+
+        public IEnumerable<float> Position { get; set; }
+        public Color Color { get; set; }
+    }
+
     internal class Program
     {
         private static IWindow window;
@@ -11,24 +29,40 @@ namespace DrawPoint
         private static readonly float[] CornflowerBlue = { 0.392f, 0.584f, 0.929f, 1.0f };
         private static readonly string VertexShaderSource = @"
         #version 440 core
-        
+
+        layout (location = 0) in vec4 Position;
+        layout (location = 1) in vec4 Color;
+
+        out VS_OUTPUT
+        {
+	        vec4 Color;
+        } OUT;
+
         void main()
         {
-            gl_Position = vec4(0, 0, 0, 1);
+	        gl_Position = Position;
+	        OUT.Color = Color;
         }
         ";
 
         private static readonly string FragmentShaderSource = @"
         #version 440 core
+
+        in VS_OUTPUT
+        {
+	        vec4 Color;
+        } IN;
+
         out vec4 Color;
 
         void main()
         {
-            Color = vec4(1.0, 0.0, 0.0, 1.0);
+	        Color = IN.Color;
         }
         ";
         private static uint Shader;
         private static uint Vao;
+        private static uint VertexBuffer;
 
         static void Main(string[] args)
         {
@@ -50,6 +84,12 @@ namespace DrawPoint
 
         private static unsafe void OnLoad()
         {
+            //IInputContext input = window.CreateInput();
+            //for (int i = 0; i < input.Keyboards.Count; i++)
+            //{
+            //    input.Keyboards[i].KeyDown += KeyDown;
+            //}
+
             Gl = GL.GetApi(window);
 
             //Creating a vertex shader.
@@ -95,7 +135,41 @@ namespace DrawPoint
             Gl.DeleteShader(vertexShader);
             Gl.DeleteShader(fragmentShader);
 
+            // Create the vertex buffer
+            // Vertex data, uploaded to the VBO.
+            //VertexPositionColor[] vertices = [
+            //    new VertexPositionColor(0.75f, -0.25f, 0.0f, Color.Red),
+            //    new VertexPositionColor(0.0f, 0.5f, 0.0f, Color.Green),
+            //    new VertexPositionColor(-0.75f, -0.25f, 0.0f, Color.Blue)
+            //];
+
+            float[] vertices = {
+                0.75f, -0.25f, 0.0f,
+                0.0f, 0.5f, 0.0f,
+                -0.75f, -0.25f, 0.0f
+            };
+
+            VertexBuffer = Gl.GenBuffer();
+            Gl.BindBuffer(BufferTargetARB.ArrayBuffer, VertexBuffer);
+            fixed (void* v = &vertices[0])
+            {
+                Gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint)(vertices.Length * sizeof(uint)), v, BufferUsageARB.StaticDraw);
+            }
             Vao = Gl.GenVertexArray();
+            Gl.BindVertexArray(Vao);
+
+            // Our stride constant. The stride must be in bytes, so we take the first attribute (a vec3), multiply it
+            // by the size in bytes of a float, and then take our second attribute (a vec2), and do the same.
+            //Gl.VertexAttribPointer((uint)VertexAttribute.Position, 4, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), null);
+
+            Gl.EnableVertexAttribArray((uint)VertexAttribute.Position);
+            //Gl.VertexAttribPointer((uint)VertexAttribute.Color, 4, VertexAttribPointerType.Float, false, 3 * sizeof(float), 1);
+            //Gl.EnableVertexAttribArray((uint)VertexAttribute.Color);
+
+            Gl.EnableVertexAttribArray(0);
+            
+            Gl.BindVertexArray(0);
         }
 
         private static unsafe void OnRender(double obj) //Method needs to be unsafe due to draw elements.
@@ -104,11 +178,12 @@ namespace DrawPoint
             {
                 Gl.ClearBuffer(Silk.NET.OpenGL.GLEnum.Color, 0, ptr);
             }
+
+            //Bind the geometry and shader.
             Gl.BindVertexArray(Vao);
             Gl.UseProgram(Shader);
-            Gl.PointSize(80.0f);
-            Gl.DrawArrays(GLEnum.Points, 0, 1);
-            Gl.BindVertexArray(0);
+            //Draw the arrays.
+            Gl.DrawArrays(GLEnum.Triangles, 0, 3);
         }
 
         private static void OnUpdate(double obj)
@@ -123,6 +198,11 @@ namespace DrawPoint
 
         private static void OnClose()
         {
+            //Remember to delete the buffers.
+            //Gl.DeleteBuffer(Vbo);
+            //Gl.DeleteBuffer(Ebo);
+            //Gl.DeleteVertexArray(Vao);
+            //Gl.DeleteProgram(Shader);
         }
     }
 }
